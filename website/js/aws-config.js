24e9676ce673f3d2ea5c44d0d42b8dec95fbb3e7
Jake Moses <mosesjake32@gmail.com>
2025-03-13 20:26:01 -0400

    updated config to point to prod, 2023 -> 2025

--- website/js/aws-config.js
+++ website/js/aws-config.js
@@ -9,17 +9,17 @@ const lambdaConfig = {
     // You can get this URL after deploying your CDK stack with:
     // aws cloudformation describe-stacks --stack-name PersonalAssistantDevResearchStack --query "Stacks[0].Outputs[?OutputKey=='ResearchGeneratorFunctionUrl'].OutputValue" --output text
     // For Prod:
-    // functionUrl: 'https://qekzfmhc3bwg4rqqyh3dyiujza0bstto.lambda-url.us-west-2.on.aws/',
+    functionUrl: 'https://qekzfmhc3bwg4rqqyh3dyiujza0bstto.lambda-url.us-west-2.on.aws/',
     // for dev
-    functionUrl: 'https://3wfy3gdqgpf3xjqkb2kurxiufa0wypxt.lambda-url.us-west-2.on.aws/',
+    // functionUrl: 'https://3wfy3gdqgpf3xjqkb2kurxiufa0wypxt.lambda-url.us-west-2.on.aws/',
 
     // Default function name (used for display purposes only)
     // You can get this name after deploying your CDK stack with:
     // aws cloudformation describe-stacks --stack-name PersonalAssistantDevResearchStack --query "Stacks[0].Outputs[?OutputKey=='ResearchGeneratorFunctionName'].OutputValue" --output text
     // For Prod:
-    // functionName: 'personal-assistant-prod-research-question-generator'
+    functionName: 'personal-assistant-prod-research-question-generator'
     // For Dev:
-    functionName: 'personal-assistant-dev-research-question-generator'
+    // functionName: 'personal-assistant-dev-research-question-generator'
 };
 
 // Function to invoke Lambda directly via Function URL
